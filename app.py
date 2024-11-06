723bd44af60ff89733ee30271470b39e1e709531
uploadsthings <71400164+uploadsthings@users.noreply.github.com>
2024-11-06 12:13:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template_string, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import re
 import nltk
 from nltk.corpus import cmudict
